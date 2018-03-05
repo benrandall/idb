219e6253f17c67459be4385f0a4aefffba61aee1
Liam DeCoste <ldecoste@csid.com>
2018-03-05 13:29:09 -0600

    Added initial react app

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
