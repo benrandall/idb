7a7f83cc52d6ff6cd45af72d1b4c341032fbdb83
Steven Diaz <stevendiaz@utexas.edu>
2018-03-05 23:38:44 -0600

    Set up bidirectional many to many relationships for all the models
    - Create script to import fixture/mock.json into the database correctly
    - Script resets and imports database into the correct models

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Config below hooks up DB running on localhost, db name is postgres
 app.config['SQLALCHEMY_DATABASE_URI'] = \
         'postgresql://localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,18 +21,57 @@ class Item(db.Model):
     market_price = db.Column(db.Integer, nullable=False)
     weight = db.Column(db.Integer, nullable=False)
 
+    videos = db.relationship('Video',
+                secondary='items_videos',
+                backref=db.backref('items_videos', lazy='dynamic'),
+                lazy='dynamic')
+
+    reddits = db.relationship('Reddit',
+                secondary='items_reddits',
+                backref=db.backref('items_reddits', lazy='dynamic'),
+                lazy='dynamic')
+
+    skills = db.relationship('Skill',
+                secondary='items_skills',
+                backref=db.backref('items_skills', lazy='dynamic'),
+                lazy='dynamic')
+
+    def __init__(self, item):
+        self.name = item['name']
+        self.examine_info = item['examine_info']
+        self.icon = item['icon']
+        self.item_type = item['type']
+        self.market_price = item['market_price']
+        self.weight = item['weight']
+
+
     def __repr__(self):
         return '<Item %r>' % self.name
 
-class RedditPost(db.Model):
-    __tablename__ = 'reddit_posts'
+class Reddit(db.Model):
+    __tablename__ = 'reddits'
 
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, unique=True, nullable=False)
     title = db.Column(db.Text, nullable=False)
 
+    skills = db.relationship('Skill',
+                secondary='skills_reddits',
+                backref=db.backref('reddits_skills', lazy='dynamic'),
+                lazy='dynamic')
+
+    items = db.relationship('Item',
+                secondary='items_reddits',
+                backref=db.backref('reddits_items', lazy='dynamic'),
+                lazy='dynamic')
+
+
+    def __init__(self, reddit):
+        self.url = reddit['url']
+        self.title = reddit['title']
+
     def __repr__(self):
-        return '<RedditPost %r>' % self.title
+        return '<Reddits %r>' % self.title
 
 
 class Video(db.Model):
@@ -42,6 +82,22 @@ class Video(db.Model):
     icon = db.Column(db.Text, nullable=False)
     video_url = db.Column(db.Text, unique=True, nullable=False)
     category = db.Column(db.Text, nullable=False)
+
+    skills = db.relationship('Skill',
+                secondary='skills_videos',
+                backref=db.backref('videos_skills', lazy='dynamic'),
+                lazy='dynamic')
+
+    items = db.relationship('Item',
+                secondary='items_videos',
+                backref=db.backref('videos_items', lazy='dynamic'),
+                lazy='dynamic')
+
+    def __init__(self, video):
+        self.name = video['name']
+        self.icon = video['icon']
+        self.category = video['category']
+        self.video_url = video['video_url']
 
     def __repr__(self):
         return '<Video %r>' % self.name
@@ -56,8 +112,59 @@ class Skill(db.Model):
     members_only = db.Column(db.Boolean, nullable=False)
     max_level = db.Column(db.Integer, nullable=False)
 
+    videos = db.relationship('Video',
+                secondary='skills_videos',
+                backref=db.backref('skills_videos', lazy='dynamic'),
+                lazy='dynamic')
+
+    reddits = db.relationship('Reddit',
+                secondary='skills_reddits',
+                backref=db.backref('skills_reddits', lazy='dynamic'),
+                lazy='dynamic')
+
+    items = db.relationship('Item',
+                secondary='items_skills',
+                backref=db.backref('items_skills', lazy='dynamic'),
+                lazy='dynamic')
+
+
+    def __init__(self, skill):
+        self.name = skill['name']
+        self.icon = skill['icon']
+        self.description = skill['description']
+        self.members_only = skill['members_only']
+        self.max_level = skill['max_level']
+
+
     def __repr__(self):
         return '<Skill %r>' % self.name
+
+# Association Tables
+items_skills = db.Table('items_skills',
+        db.Column('item_id', db.Integer, db.ForeignKey('items.id')),
+        db.Column('skill_id', db.Integer, db.ForeignKey('skills.id')),
+    )
+
+items_videos = db.Table('items_videos',
+        db.Column('item_id', db.Integer, db.ForeignKey('items.id')),
+        db.Column('video_id', db.Integer, db.ForeignKey('videos.id')),
+    )
+
+items_reddits = db.Table('items_reddits',
+        db.Column('item_id', db.Integer, db.ForeignKey('items.id')),
+        db.Column('reddit_id', db.Integer, db.ForeignKey('reddits.id')),
+    )
+
+skills_videos = db.Table('skills_videos',
+        db.Column('skill_id', db.Integer, db.ForeignKey('skills.id')),
+        db.Column('videos', db.Integer, db.ForeignKey('videos.id')),
+    )
+
+skills_reddits = db.Table('skills_reddits',
+        db.Column('skill_id', db.Integer, db.ForeignKey('skills.id')),
+        db.Column('reddit_id', db.Integer, db.ForeignKey('reddits.id')),
+    )
+
 
 if __name__ == "__main__":
     app.run()
