ef6c72257c5182896c2c84f835672e8a5b098e7f
Luke Wright <luke@simplyluke.com>
2018-03-19 13:57:46 -0500

    Add postman collection with API tests, json errors from backend (#108)
    * json errors for postman tests - closes #107
    
    * add exported postman collection with all associated tests - closes #82
    
    * remove extra import
    
    * correct postman filename per rubric

--- backend/main.py
+++ backend/main.py
@@ -271,6 +271,15 @@ def about():
         }
     return jsonify(result)
 
+# App error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(error=404, text=str(e)), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify(error=500, text=str(e)), 500
+
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.run()
