f08e3c7ed3a234aca560709e39f4d5cbe531cb7a
Claire Bingham <clairebingham@utexas.edu>
2018-03-20 21:39:43 -0500

    add horizontal rules to make headers look better

--- frontend/runescrape/src/components/RSContainer/RSContainer.js
+++ frontend/runescrape/src/components/RSContainer/RSContainer.js
@@ -9,7 +9,7 @@ const RSContainer = (props) => {
             <Col sm='12'>
                 <h1>{ props.title }
                     { props.subtitle && <small className="text-muted"> { props.subtitle }</small>}
-                </h1>
+                </h1><hr/>
                 <p>{ props.body }</p>
             </Col>
         </Row>
