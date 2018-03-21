06acd15f734301284ee4e6644ec94ef1ebad2347
Luke Wright <luke@simplyluke.com>
2018-03-20 22:52:20 -0500

    tests running with db imported

--- backend/tests.py
+++ backend/tests.py
@@ -1,14 +1,28 @@
+import os
+import unittest
 from flask import Flask
 from flask_testing import TestCase
 from main import create_app, db
 
-class IdbTests(TestCase):
 
-     def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
+class IdbTests(TestCase):
+    def create_app(self):
+        # in-memory db
+        app = create_app('sqlite://', True)
         return app
-    pass
+
+    def setUp(self):
+        # set up data from our fixtures
+        import import_fixture
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_home_response(self):
+        response = self.client.get('/')
+        self.assert200(response)
+
 
 if __name__ == '__main__':
     unittest.main()
