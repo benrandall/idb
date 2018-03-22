886bdb7d5704f3110ce6bba5a9856648187d9e2b
Steven Diaz <stevendiaz@utexas.edu>
2018-03-22 06:41:01 +0000

    Updated docker files

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,14 @@
 from main import create_app
 
-app = create_app('postgresql://localhost/postgres', debug=True)
+user = os.environ['POSTGRES_USER']
+pwd = os.environ['POSTGRES_PASSWORD']
+db = os.environ['POSTGRES_DB']
+host = 'dbpostgres'
+port = '5432'
+
+production_uri = 'postgresql://%s:%s@%s:%s/%s' % (user, pwd, host, port, db)
+
+app = create_app(production_uri, debug=True)
 
 if __name__ == "__main__":
     app.run()
