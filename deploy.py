4bcc724df1b00a95c92442b6c7117238b980cbb5
Liam DeCoste <liam@mediahack.com>
2018-03-22 02:19:32 -0500

    Added deployment script

---
+++ deploy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from shutil import copyfile
+from subprocess import call
+
+def run(command: str) -> None:
+    print("$", command)
+    call(command.split())
+
+def main():
+    print('---- Deploying Runescrape ----')
+    env = os.environ.get('STAGE')
+
+    if 'base' in sys.argv:
+        print('---- Building Base Image ----')
+        run('sudo docker build --no-cache -f Dockerfile-Base -t runescrape-base:latest .')
+
+    if 'app' in sys.argv:
+        print('---- Building Application Image ----')
+        run('sudo docker build --no-cache -f Dockerfile-Runescrape -t runescrape:latest . --build-arg REACT_ENV=%s' % env)
+
+    if 'install-schema' in sys.argv:
+        print('---- Installing Database Schema ----')
+        run('sudo docker exec -it idb_web_1 sh -c "python3 import_fixture.py"')
+
+if __name__ == "__main__":
+    main()
