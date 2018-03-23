678b2eaf8d7358374814bfea03b39d064620b66f
Claire Bingham <clairebingham@utexas.edu>
2018-03-22 21:28:24 -0500

    fix file locations

--- frontend/test/.setup.js
+++ frontend/test/.setup.js
@@ -1,121 +1,48 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { expect } from 'chai';
-import * as sinon from 'sinon'
-import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+require('babel-register')();
 
-import App from '../src/components/App';
-import Home from '../src/components/Home';
-import RSAboutPage from '../src/components/RSAboutPage/RSAboutPage';
-import RSContainer from '../src/components/RSContainer/RSContainer';
-import RSLink from '../src/components/RSLink/RSLink';
-import RSRedditCard from '../src/components/RSRedditCard/RSRedditCard';
-import RSTeamMemeber from '../src/components/RSTeamMemeber/RSTeamMemeber';
-import RSTool from '../src/components/RSTool/RSTool';
-import RSVideoCard from '../src/components/RSVideoCard/RSVideoCard';
-import RSVideoDetailPage from '../src/components/RSVideoDetailPage/RSVideoDetailPage';
-import SkillDetailPage from '../src/components/SkillDetailPage/SkillDetailPage';
-import ItemDetailPage from '../src/components/SkillDetailPage/SkillDetailPage';
-import GitHubStats from '../src/components/SkillDetailPage/SkillDetailPage';
-import CommunityGrid from '../src/components/SkillDetailPage/SkillDetailPage';
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
+const { document } = (new JSDOM('')).window;
+global.document = document;
 
-// App
-describe('<App/>', function () {
-    it('should render successfully', function () {
-        shallow(<App />);
-    });
-    it('should render the navigation bar', function () {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('.navbar').exists()).to.be.equal(true)
-    });
-    it ('should have 4 navigation items', function () {
-        const wrapper = shallow(<App />);
-        const tabs = wrapper.find('Nav').children();
-        expect(tabs.to.have.length(4));
+var exposedProperties = ['window', 'navigator', 'document'];
 
-    });
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
 });
 
-// write tests below
-describe('<Home />', () => {
-  it('should render', () => {
-    shallow(<Home />);
-  });
-  it('should render the carousel item', () => {
-    const wrapper = shallow(<Home />)
-    expect(wrapper.find('.carousel').exists()).to.eql(true)
-  });
-});
+global.navigator = {
+  userAgent: 'node.js'
+};
 
-describe('<RSAboutPage/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSAboutPage />);
-    })
-});
+documentRef = document;
 
-describe('<RSContainer/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSContainer />);
-    })
-});
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
 
-describe('<RSLink/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSLink />);
-    })
-});
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
 
-describe('<RSRedditCard/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSRedditCard />);
-    })
-});
-
-describe('<RSTeamMemeber/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSTeamMemeber />);
-    })
-});
-
-describe('<RSTool/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSTool />);
-    })
-});
-
-describe('<RSVideoCard/>', function () {
-    it('should render successfully', function () {
-        shallow(<RSVideoCard />);
-    })
-});
-
-describe('<RSVideoDetailPage/>', function () {
-    it('should render successfully', function () {
-       shallow(<RSVideoDetailPage />);
-    })
-});
-
-describe('<SkillDetailPage/>', function () {
-    it('should render successfully', function () {
-        shallow(<SkillDetailPage />);
-    })
-});
-
-describe('<ItemDetailPage/>', function () {
-    it('should render successfully', function () {
-        shallow(<ItemDetailPage />);
-    })
-});
-
-describe('<GitHubStats/>', function () {
-    it('should render successfully', function () {
-        shallow(<GitHubStats />);
-    })
-});
-
-describe('<CommunityGrid/>', function () {
-    it('should render successfully', function () {
-        shallow(<CommunityGrid />);
-    })
-});
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
