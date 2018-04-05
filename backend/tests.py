fe6d2d24af240a2fa0efb5800377f620d410e5a3
Luke Wright <luke@simplyluke.com>
2018-04-05 18:47:41 -0500

    Write better unit tests (#173)

--- backend/tests.py
+++ backend/tests.py
@@ -1,6 +1,8 @@
 import unittest
+import os
 import json
 from app import create_app, db
+from app.api.github import GithubApiWrapper
 from app.models import Item, Reddit, Skill, Video
 from app.api.github import GithubApiWrapper
 from config import Config
@@ -11,9 +13,10 @@ class TestConfig(Config):
     ELASTICSEARCH_URL = None
 
 class IdbTests(unittest.TestCase):
-
     def setUp(self):
         self.app = create_app(TestConfig)
+        self.app.testing = True
+        self.client = self.app.test_client()
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -28,6 +31,98 @@ class IdbTests(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
+    def test_skill_model(self):
+        skill = Skill({'name': "name",
+                      'icon': "fakeicon.png",
+                      'description': "fake desc",
+                      'members_only': False,
+                      'max_level': 99,
+                      'skill_type': 'test'})
+        db.session.add(skill)
+        db.session.commit()
+        skills = Skill.query.all()
+        assert skill in skills
+        assert str(skill) == "<Skill 'name'>"
+        assert skill.toJSON(False) == {'id': 1,
+                                       'name': 'name',
+                                       'icon': 'fakeicon.png',
+                                       'description': 'fake desc',
+                                       'members_only': False,
+                                       'max_level': 99}
+        assert len(skills) == 1
+
+    def test_item_model(self):
+        item = Item({'name': "name",
+                     'icon': "fakeicon.png",
+                     'examine_info': "fake info",
+                     'type': "fake type",
+                     'market_price': 99,
+                     'weight': 0,
+                     'members_only': True,
+                     'equipable': True,
+                     'quest_item': False})
+        db.session.add(item)
+        db.session.commit()
+        items = Item.query.all()
+        assert item in items
+        assert str(item) == "<Item 'name'>"
+        assert item.toJSON(False) == {'id': 1,
+                                      'name': 'name',
+                                      'icon': "fakeicon.png",
+                                      'examine_info': "fake info",
+                                      'item_type': "fake type",
+                                      'market_price': 99,
+                                      'weight': 0}
+        assert len(items) == 1
+
+    def test_reddit_model(self):
+        reddit = Reddit({'url': "http://localhost:5000",
+                         'title': 'Reddit title',
+                         'category': 'reddit',
+                         'community_type': 'subreddit'})
+
+        db.session.add(reddit)
+        db.session.commit()
+        reddits = Reddit.query.all()
+        assert reddit in reddits
+        assert str(reddit) == "<Reddits 'Reddit title'>"
+        assert reddit.toJSON(False) == {'id': 1,
+                                        'url': 'http://localhost:5000',
+                                        'title': 'Reddit title',
+                                        'category': 'reddit',
+                                        'community_type': 'subreddit'}
+        assert len(reddits) == 1
+
+    def test_video_model(self):
+        video = Video({'name': "Video Name",
+                       'icon': 'fakeicon.png',
+                       'video_url': "http://youtube.com/",
+                       'category': 'runescape',
+                       'community_type': 'youtube'})
+
+        db.session.add(video)
+        db.session.commit()
+        videos = Video.query.all()
+        assert video in videos
+        assert str(video) == "<Video 'Video Name'>"
+        assert video.toJSON(False) == {'id': 1,
+                                       'name': "Video Name",
+                                       'icon': 'fakeicon.png',
+                                       'video_url': "http://youtube.com/",
+                                       'category': 'runescape',
+                                       'community_type': 'youtube'}
+        assert len(videos) == 1
+
+    def test_404_response(self):
+        resp = self.client.get('/badreq')
+        assert resp.headers['content-type'] == 'application/json'
+        assert resp.status_code == 404
+
+    def test_images(self):
+        resp = self.client.get('/images/luke.jpg')
+        assert resp.headers['content-type'] == 'image/jpeg'
+        assert resp.status_code == 200
 
     def test_github_strip_page_count(self):
         gh = GithubApiWrapper(owner='benrandall', repo='idb', token=self.app.config['GITHUB_API_TOKEN'])
