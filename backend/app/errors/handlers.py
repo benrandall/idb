51febb2d263f6d3a30c54d8261f3144a34444ca4
Luke Wright <luke@simplyluke.com>
2018-04-04 23:34:39 -0500

    update postman tests to be green with new API

--- backend/app/errors/handlers.py
+++ backend/app/errors/handlers.py
@@ -1,12 +1,11 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import db
 from app.errors import bp
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def page_not_found(e):
     return jsonify(error=404, text=str(e)), 404
 
 @bp.errorhandler(500)
 def internal_server_error(e):
     return jsonify(error=500, text=str(e)), 500
-
