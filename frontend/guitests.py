083749982f370d19b4782102719769f88d8e0793
Dylan Ramage <dylanlramage@utexas.edu>
2018-04-05 21:50:38 -0500

    Selenium tests updated (#178)
    * updating test
    
    * Updated unit tests
    
    * updated selenium tests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -13,64 +13,44 @@ class PythonOrgSearch(unittest.TestCase):
         driver.get("http://runescrape.lol")
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/a[2]').click()
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/a[1]').click()
-        driver.find_element_by_xpath('//*[@id="root"]/div/div/div')
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/nav/div/ul/li[5]/div/div').is_displayed()
 
     def test_items_page(self):
         driver = self.driver
-        driver.get("http://runescrape.lol")
-        driver.find_element_by_xpath('//*[@id="root"]/div/div/nav/div/ul/li[1]/a').click()
-        driver.implicitly_wait(60)
-        driver.find_element_by_class_name('card').click()
-        driver.implicitly_wait(10)
+        driver.get("http://runescrape.lol/items")
         assert driver.current_url.find("items") != -1
-        driver.find_element_by_class_name('icon')
-        driver.find_element_by_class_name('medium-title')
-        driver.find_element_by_class_name('small-title')
-        driver.find_elements_by_class_name('card')
-        driver.find_element_by_xpath('//*[@id="item"]/div[3]/div/a/div/div[2]/h5').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("skills") != -1
-        driver.back()
-        driver.implicitly_wait(10)
-        driver.find_element_by_xpath('//*[@id="item"]/div[5]/div[1]/div/div/a/button').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("reddit") != -1
-        driver.back()
-        driver.implicitly_wait(10)
-        driver.find_element_by_xpath('//*[@id="item"]/div[5]/div[2]/div/div/a/button').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("community") != -1
-        driver.back()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[2]/div[1]/a/div/div[1]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[4]/ul/li[4]/a').is_displayed()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[2]/div[1]/a/div/div[1]').click()
+        driver.implicitly_wait(30)
+        assert driver.current_url.find("items/1")
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[1]/div/div/div[1]/img').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[1]/div/div/div[2]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[2]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[3]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[4]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="item"]/div[5]').is_displayed()
 
     def test_skills_page(self):
         driver = self.driver
-        driver.get("http://runescrape.lol")
-        driver.find_element_by_xpath('//*[@id="root"]/div/div/nav/div/ul/li[2]/a').click()
-        driver.implicitly_wait(60)
-        driver.find_element_by_class_name('card').click()
-        driver.implicitly_wait(10)
+        driver.get("http://runescrape.lol/skills")
         assert driver.current_url.find("skills") != -1
-        driver.find_element_by_class_name('medium-title')
-        driver.find_element_by_class_name('small-title')
-        driver.find_elements_by_class_name('card')
-        driver.find_element_by_xpath('//*[@id="skill"]/div[3]/div/a/div/div[1]').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("items") != -1
-        driver.back()
-        driver.implicitly_wait(10)
-        driver.find_element_by_xpath('//*[@id="skill"]/div[5]/div[1]/div/div/a/button').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("reddit") != -1
-        driver.back()
-        driver.implicitly_wait(10)
-        driver.find_element_by_xpath('//*[@id="skill"]/div[5]/div[2]/div/div/a/button').click()
-        driver.implicitly_wait(10)
-        assert driver.current_url.find("community") != -1
-        driver.back()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[2]/div[1]/a/div/div[1]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[4]/ul/li[4]/a').is_displayed()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[2]/div[1]/a/div/div[1]').click()
+        driver.implicitly_wait(30)
+        assert driver.current_url.find("skills/1")
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[1]/div/div/div[1]/img').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[1]/div/div/div[2]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[2]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[3]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[4]').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="skill"]/div[5]').is_displayed()
 
     def test_community_page(self):
         driver = self.driver
-        driver.get("http://runescrape.lol")
+        driver.get("http://runescrape.lol/community")
         driver.find_element_by_xpath('//*[@id="root"]/div/div/nav/div/ul/li[3]/a').click()
         driver.implicitly_wait(40)
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[1]/div/div/a/button').click()
@@ -106,9 +86,9 @@ class PythonOrgSearch(unittest.TestCase):
         assert driver.current_url.find("about") != -1
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[1]/div/p/p').is_displayed()
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div').is_displayed()
-        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/h4/text()').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/h4').is_displayed()
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/h4/small').is_displayed()
-        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/p[1]/text()').is_displayed()
+        assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/p[1]').is_displayed()
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/p[2]/b').is_displayed()
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/p[3]/b').is_displayed()
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/p/div/div[1]/p[4]/b').is_displayed()
@@ -117,8 +97,10 @@ class PythonOrgSearch(unittest.TestCase):
         assert driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[3]/div/p/div/div/p[3]/b').is_displayed()
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[6]/div/p/div/div[1]/a').click()
         assert driver.current_url.find("report") != -1
+        driver.back()
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[6]/div/p/div/div[2]/a').click()
         assert driver.current_url.find("api") != -1
+        driver.back()
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[6]/div/p/div/div[3]/a').click()
         assert driver.current_url.find("idb") != -1
 
