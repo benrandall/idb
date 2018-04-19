b4ab5aebf37b97924ef0489cf865962ef21b19b7
Claire Bingham <clairebingham@utexas.edu>
2018-04-19 16:30:27 -0500

    delete root app/ and frontend/runescrape

--- app/static/js/skill.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommunityGrid from "./components/CommunityGrid";
-
-ReactDOM.render(
-    <CommunityGrid type="skills" id={ document.getElementById('skill').getAttribute('data-id') }/>,
-    document.getElementById('community')
-);
--- app/static/js/skills.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CardGrid from './components/CardGrid.js'
-
-ReactDOM.render(
-    <CardGrid cardType="skills" data={ document.getElementById('card-grid').getAttribute('card-data') }/>,
-    document.getElementById('card-grid')
-);
